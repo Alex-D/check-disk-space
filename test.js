d1ed64e5251d0ccdd1f307f3ff7b2d47545df1a3
YanDev <1526048+YanDevDe@users.noreply.github.com>
2021-02-21 22:33:01 +0100

    forgot to remove param

--- test.js
+++ test.js
@@ -133,7 +133,7 @@ function mockCheckDiskSpace(platform, fixture) {
   Object.defineProperty(process, 'platform', {value: platform})
 
   // Mock child_process.execFile
-  require('child_process').execFile = (file, args, param, callback) => {
+  require('child_process').execFile = (file, args, callback) => {
     process.nextTick(() => {
       callback(fixture.execError, fixture.execOutput)
     })
