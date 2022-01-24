8d1889109fff96da4b276723f45c5757d447c424
Alex-D <contact@alex-d.fr>
2022-01-25 00:22:29 +0100

    lint: ignore no-console in demo.js

--- demo.js
+++ demo.js
@@ -3,5 +3,6 @@ const checkDiskSpace = require('./dist/check-disk-space.cjs').default
 const isWindows = process.platform === 'win32'
 
 checkDiskSpace(isWindows ? 'C:/' : '/').then((diskSpace) => {
+	// eslint-disable-next-line no-console
 	console.log(diskSpace)
 })
