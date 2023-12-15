43f5a0f5b00ed3a80db62dc089116d3bbc0c704c
Brett <27568879+BrettCleary@users.noreply.github.com>
2023-12-14 16:06:53 -0800

    rm hyperplay, add platform check for tests, rm normalize

--- test/functions/getFirstExistingParentPath.spec.ts
+++ test/functions/getFirstExistingParentPath.spec.ts
@@ -1,4 +1,4 @@
-import { normalize } from 'node:path'
+import { platform } from 'node:os'
 
 import test from 'ava'
 import { PathLike } from 'fs'
@@ -6,39 +6,44 @@ import { PathLike } from 'fs'
 import getFirstExistingParentPath from '@/src/functions/getFirstExistingParentPath'
 import mockDependencies from '@/test/__helpers__/mockDependencies'
 
-const getMockedDependencies = (parentPath: string) => mockDependencies({
+
+const getDependencies = (parentPath: string) => mockDependencies({
 	fsAccess: async (directoryPath: PathLike) => directoryPath === parentPath ? Promise.resolve() : Promise.reject(new Error('File does not exists')),
 })
 
-test('unix: get first existing parent path', async t => {
-	const parentPath = normalize('/home/Alex')
+const os = platform()
+const isWindows = os === 'win32'
+const testIf = (condition: boolean) => (condition ? test : test.skip)
+
+testIf(!isWindows)('unix: get first existing parent path', async t => {
+	const parentPath = '/home/Alex'
 	const dependencies = getDependencies(parentPath)
 
 	t.is(await getFirstExistingParentPath('/home/Alex/games/Some/Game', dependencies), parentPath)
 })
 
-test('unix: get first parent can be the path itself', async t => {
-	const parentPath = normalize('/home/Alex')
+testIf(!isWindows)('unix: get first parent can be the path itself', async t => {
+	const parentPath = '/home/Alex'
 	const dependencies = getDependencies(parentPath)
 
 	t.is(await getFirstExistingParentPath(parentPath, dependencies), parentPath)
 })
 
-test('win32: Gets parent to C:\\HyperPlay', async t => {
-	const parentPath = normalize('C:\\')
+testIf(isWindows)('win32: Gets parent to C:\\Alex', async t => {
+	const parentPath = 'C:\\'
 	const dependencies = getDependencies(parentPath)
-	t.is(await getFirstExistingParentPath(normalize('C:\\HyperPlay'), dependencies), parentPath)
+	t.is(await getFirstExistingParentPath('C:\\Alex', dependencies), parentPath)
 })
 
-test('win32: Returns root folder when called on root folder', async t => {
-	const parentPath = normalize('C:\\')
+testIf(isWindows)('win32: Returns root folder when called on root folder', async t => {
+	const parentPath = 'C:\\'
 	const dependencies = getDependencies(parentPath)
 	t.is(await getFirstExistingParentPath(parentPath, dependencies), parentPath)
 })
 
-test('win32: returns empty string when drive does not exist', async t => {
-	const drivePathThatExists = normalize('C:\\')
+testIf(isWindows)('win32: returns empty string when drive does not exist', async t => {
+	const drivePathThatExists = 'C:\\'
 	const dependencies = getDependencies(drivePathThatExists)
-	const drivePathThatDoesNotExist = normalize('Z:\\')
+	const drivePathThatDoesNotExist = 'Z:\\'
 	t.is(await getFirstExistingParentPath(drivePathThatDoesNotExist, dependencies), '')
 })
