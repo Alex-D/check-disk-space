c69f184a7dcc9ea7c71baa7a9fa5dab7504f5457
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2023-12-14 16:38:11 -0800

    update tests to use '/'

--- test/functions/getFirstExistingParentPath.spec.ts
+++ test/functions/getFirstExistingParentPath.spec.ts
@@ -1,49 +1,50 @@
-import { platform } from 'node:os'
-
 import test from 'ava'
 import { PathLike } from 'fs'
 
 import getFirstExistingParentPath from '@/src/functions/getFirstExistingParentPath'
 import mockDependencies from '@/test/__helpers__/mockDependencies'
 
-
 const getDependencies = (parentPath: string) => mockDependencies({
 	fsAccess: async (directoryPath: PathLike) => directoryPath === parentPath ? Promise.resolve() : Promise.reject(new Error('File does not exists')),
 })
 
-const os = platform()
-const isWindows = os === 'win32'
-const testIf = (condition: boolean) => (condition ? test : test.skip)
-
-testIf(!isWindows)('unix: get first existing parent path', async t => {
+test('unix: get first existing parent path', async t => {
 	const parentPath = '/home/Alex'
 	const dependencies = getDependencies(parentPath)
 
 	t.is(await getFirstExistingParentPath('/home/Alex/games/Some/Game', dependencies), parentPath)
 })
 
-testIf(!isWindows)('unix: get first parent can be the path itself', async t => {
+test('unix: get first parent can be the path itself', async t => {
 	const parentPath = '/home/Alex'
 	const dependencies = getDependencies(parentPath)
 
 	t.is(await getFirstExistingParentPath(parentPath, dependencies), parentPath)
 })
 
-testIf(isWindows)('win32: Gets parent to C:\\Alex', async t => {
-	const parentPath = 'C:\\'
+test('unix: get first parent of root is root', async t => {
+	const parentPath = '/'
 	const dependencies = getDependencies(parentPath)
-	t.is(await getFirstExistingParentPath('C:\\Alex', dependencies), parentPath)
+
+	t.is(await getFirstExistingParentPath(parentPath, dependencies), parentPath)
 })
 
-testIf(isWindows)('win32: Returns root folder when called on root folder', async t => {
-	const parentPath = 'C:\\'
+test('win32: Gets parent to C:\\Alex', async t => {
+	// note that 'C:/' will fail on UNIX os's as normalize(C:/Alex/..) = C: not C:/
+	const parentPath = 'C:'
+	const dependencies = getDependencies(parentPath)
+	t.is(await getFirstExistingParentPath('C:/Alex', dependencies), parentPath)
+})
+
+test('win32: Returns root folder when called on root folder', async t => {
+	const parentPath = 'C:/'
 	const dependencies = getDependencies(parentPath)
 	t.is(await getFirstExistingParentPath(parentPath, dependencies), parentPath)
 })
 
-testIf(isWindows)('win32: returns empty string when drive does not exist', async t => {
-	const drivePathThatExists = 'C:\\'
+test('win32: returns empty string when drive does not exist', async t => {
+	const drivePathThatExists = 'C:/'
 	const dependencies = getDependencies(drivePathThatExists)
-	const drivePathThatDoesNotExist = 'Z:\\'
+	const drivePathThatDoesNotExist = 'Z:/'
 	t.is(await getFirstExistingParentPath(drivePathThatDoesNotExist, dependencies), '')
 })
